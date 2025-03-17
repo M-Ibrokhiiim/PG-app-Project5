96454114c62b7d30e3bae112f1761f15e8074357
M_Ibrokhim <ibrokhimmuysinov@gmail.com>
2025-03-17 11:52:56 +0300

    99%

--- inndex.js
+++ inndex.js
@@ -11,18 +11,26 @@ let password=""
 
 const inputField=document.getElementById('random-password-id')
 
+
+// Function for generate passwords
 function generateRandomPassword(){
+
     password+=alphabetsL[Math.floor(Math.random()*alphabetsL.length)]
     password+=alphabetsU[Math.floor(Math.random()*alphabetsU.length)]
     password+=numbers[Math.floor(Math.random()*numbers.length)]
     password+=simbols[Math.floor(Math.random()*simbols.length)]
     
-    
     while(passwordLength>password.length){
         password+=all[Math.floor(Math.random()*all.length)]
     }
+
     inputField.value=password    
     password=''
+
 }
 
-console.log(password);
+// Function for take value of input
+function copyField(){
+    inputField.select()
+    document.execCommand('copy')
+}
